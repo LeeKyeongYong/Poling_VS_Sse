974575b61c8c45f410d7a2182bd34506c821f03b
이경용 <sleekydz86@icloud.com>
2024-02-07 22:18:37 +0900

    미니채팅 다른버전 셋팅 완료

---
+++ sse_chat/src/main/java/com/example/schat/SseChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.schat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseChatApplication.class, args);
+    }
+
+}
---
+++ sse_chat/src/test/java/com/example/schat/SseChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
