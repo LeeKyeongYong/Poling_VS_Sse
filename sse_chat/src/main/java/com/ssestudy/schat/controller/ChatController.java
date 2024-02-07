7f869b30b452da4bbf1a8e486c6a8241075a2def
이경용 <sleekydz86@icloud.com>
2024-02-07 23:55:35 +0900

    POST /chat/writeMessage

--- sse_chat/src/main/java/com/ssestudy/schat/controller/ChatController.java
+++ sse_chat/src/main/java/com/ssestudy/schat/controller/ChatController.java
@@ -1,4 +1,16 @@
 package com.ssestudy.schat.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/chat")
 public class ChatController {
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public String writeMessage(){
+        return "메세지가 작성하였습니다.";
+    }
 }
