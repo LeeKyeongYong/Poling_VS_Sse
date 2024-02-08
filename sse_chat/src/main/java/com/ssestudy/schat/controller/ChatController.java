e7a56092d682251e6ace9dba61630317f73774e2
이경용 <sleekydz86@icloud.com>
2024-02-08 13:28:56 +0900

    작성내용을 json으로 전달

--- sse_chat/src/main/java/com/ssestudy/schat/controller/ChatController.java
+++ sse_chat/src/main/java/com/ssestudy/schat/controller/ChatController.java
@@ -3,10 +3,7 @@ package com.ssestudy.schat.controller;
 import com.ssestudy.schat.comp.ChatMessage;
 import com.ssestudy.schat.rsdata.RsData;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +16,14 @@ public class ChatController {
 
     public record WriteMessageResponse(long id){}
 
+    public record WriteMessageRequest(String authrName,String content){}
+
+
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<WriteMessageResponse> writeMessage(){
-        ChatMessage message = new ChatMessage("홍길동","안녕하세요.");
+    public RsData<WriteMessageResponse> writeMessage(@RequestBody WriteMessageRequest req){
+        ChatMessage message = new ChatMessage(req.authrName(),req.content());
+        chatMessages.add(message);
         return new RsData<>(
                 "S-1",
                 "메세지가 작성되었습니다.",
