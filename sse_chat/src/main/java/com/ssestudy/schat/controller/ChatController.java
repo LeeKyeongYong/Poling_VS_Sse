d479c8221b0ad8c9b898af22b20fdfe5c6df5b93
이경용 <sleekydz86@icloud.com>
2024-02-08 13:35:02 +0900

    MessagesResponse 클래스

--- sse_chat/src/main/java/com/ssestudy/schat/controller/ChatController.java
+++ sse_chat/src/main/java/com/ssestudy/schat/controller/ChatController.java
@@ -31,13 +31,14 @@ public class ChatController {
         );
     }
 
+    private record MessagesResponse(List<ChatMessage> messages,long count){}
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<List<ChatMessage>> messages(){
-        return RsData<>(
+    public RsData<MessagesResponse> messages(){
+        return new RsData<>(
                     "S-1",
                     "성공",
-                    chatMessages
+                    new MessagesResponse(chatMessages,chatMessages.size())
                 );
     }
 }
